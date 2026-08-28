123bfab60a8de68db230eefad4fc2ecf1078b342
rifqijoki <rifqiramajoki@gmail.com>
2026-08-28 12:44:58 +0700

    fix auth controller case conflict

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
 {
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('auth.login-page');
     }
 
     public function login(Request $request)
@@ -21,12 +21,25 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
+
             return redirect()->intended('/');
         }
 
         return back()->withErrors([
             'email' => 'Email atau password salah.',
         ])->onlyInput('email');
+    }
+
+    // Halaman register pembeli
+    public function showRegisterForm()
+    {
+        return view('auth.register-pembeli');
+    }
+
+    // Halaman register penjual
+    public function showRegisterPenjualForm()
+    {
+        return view('auth.register-penjual');
     }
 
     public function logout(Request $request)

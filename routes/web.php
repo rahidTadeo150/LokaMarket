7589106f65c492db0d5dafd85dc3caa80b633be2
Okta Hidayat <oktaayt@gmail.com>
2026-08-28 10:20:09 +0700

    added register page

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login-user', [authController::class, 'loginPage'])->name('cust.login');
 
 Route::get('/', [UserController::class, 'landingPage'])->name('cust.landingPage');
-<<<<<<< HEAD
-Route::get('/cara-kerja', [UserController::class, 'caraKerja'])->name('cust.caraKerja');
+Route::get('/cara-kerja', [UserController::class, 'caraKerjaPage'])->name('cust.caraKerja');
+Route::get('/tentang-kami', [UserController::class, 'tentangKamiPage'])->name('cust.tentangKami');
+
 // Tampilan Form Pembeli
 Route::get('/register', function () {
     return view('auth.register-pembeli');
@@ -18,8 +19,6 @@ Route::get('/register', function () {
 Route::get('/register-penjual', function () {
     return view('auth.register-penjual');
 });
-=======
-Route::get('/cara-kerja', [UserController::class, 'caraKerjaPage'])->name('cust.caraKerja');
-Route::get('/tentang-kami', [UserController::class, 'tentangKamiPage'])->name('cust.tentangKami');
 
->>>>>>> a9fadc9fc8a23b162e0ef6b73aaa790300980237
+
+
